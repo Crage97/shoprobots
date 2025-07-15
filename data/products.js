8b0041b2e966d424b17c1a94946d192f10dc4274
Courage Nzvimbo <37210281+Crage97@users.noreply.github.com>
2025-07-15 14:24:03 +0200

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id: 1,
+    name: "Industrial Valve",
+    description: "High pressure valve for mining operations.",
+    image: "/valve.jpg"
+  },
+  {
+    id: 2,
+    name: "Office Chair",
+    description: "Ergonomic chair for office use.",
+    image: "/chair.jpg"
+  }
+];
+
+export default products;
